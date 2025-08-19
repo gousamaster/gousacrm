b4ca8fef16a34007a99f24df1f18f014194bcd65
gousamaster <master.gousa@gmail.com>
2025-08-18 21:56:09 -0400

    feat: Update site header

--- src/components/shared/site-header.tsx
+++ src/components/shared/site-header.tsx
@@ -11,19 +11,7 @@ export function SiteHeader() {
                     orientation="vertical"
                     className="mx-2 data-[orientation=vertical]:h-4"
                 />
-                <h1 className="text-base font-medium">BETA V.0.0.82</h1>
-                <div className="ml-auto flex items-center gap-2">
-                    {/* <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
-                        <a
-                            href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            className="dark:text-foreground"
-                        >
-                            GitHub
-                        </a>
-                    </Button> */}
-                </div>
+                <h1 className="text-base font-medium">V.0.1.0</h1>
             </div>
         </header>
     )
